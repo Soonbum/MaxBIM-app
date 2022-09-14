37896e4f54800ed9f2249971fd8ee372c4f9a4c4
Soonbum Jeong <peacemaker84@gmail.com>
2022-09-14 14:03:45 +0900

    MaxBIM-app (2022.09.14 - 14:01)
    수정중
    1. PDF에 텍스트 붙이기 프로그램
      - 레이어 이름에 대한 설명도 첨부하도록 할 것

--- MaxBIM-app/module/attachTextOnPdf.js
+++ MaxBIM-app/module/attachTextOnPdf.js
@@ -6,9 +6,11 @@ const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib;
 
 const fileButton = document.getElementById("file");
 const saveButton = document.getElementById("save");
+const csvButton = document.getElementById("csv");
 
 saveButton.addEventListener("click", savePDF);
 
+// PDF 파일 저장하기
 async function savePDF() {
     const selectedFiles = fileButton.files;
 
@@ -17,19 +19,18 @@ async function savePDF() {
         return;
     }
 
+    // 레이어 체계에 대한 정의를 가져옴
+    let layer = new LayerNameSystem();
+
     // NanumGothic.ttf
     for(const file of selectedFiles) {
         if (file.type == "application/pdf") {
             // 기존 PDF 파일 가져오기
-            //const url = (`http://127.0.0.1:8887/${file.name}`);
-            const url = (`C:\\${file.name}`);
-            const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
+            const fileUrl = (`C:\\${file.name}`);
+            const existingPdfBytes = await fetch(fileUrl).then(res => res.arrayBuffer());
 
             // 기존 PDF 파일로부터 PDFDocument를 로드함
             const pdfDoc = await PDFDocument.load(existingPdfBytes);
-
-            // Courier 글꼴 포함시킴
-            const helvetica = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
             const fontUrl = 'NanumGothic.ttf';
             const fontBytes = await fetch(fontUrl).then((res) => res.arrayBuffer());
@@ -62,6 +63,7 @@ async function savePDF() {
     }
 }
 
+// 문자열 Ansi -> Unicode로 변환
 function charToUnicode(str) {
     if(!str)   return false;
     let unicode = '';
@@ -72,4 +74,62 @@ function charToUnicode(str) {
     }
 
     return unicode;
+}
+
+// 레이어 시스템 클래스
+class LayerNameSystem {
+    constructor() {
+        let bExtendedLayer = false;     // 확장 레이어(true), 기본 레이어(false)
+
+        // 1. 공사 구분 (필수)
+        let code_name = [];     // 코드 이름
+        let code_desc = [];     // 코드 설명
+
+        // 2. 동 구분 (필수)
+        let dong_name = [];     // 동 이름
+        let dong_desc = [];     // 동 설명
+
+        // 3. 층 구분 (필수)
+        let floor_name = [];    // 층 이름
+        let floor_desc = [];    // 층 설명
+
+        // 4. 타설번호 (필수)
+        let cast_name = [];
+
+        // 5. CJ 구간 (필수)
+        let CJ_name = [];
+
+        // 6. CJ 속 시공순서 (필수)
+        let orderInCJ_name = [];
+
+        // 7. 부재 구분 (필수)
+        let obj_name = [];      // 부재 이름
+        let obj_desc = [];      // 부재 설명
+        let obj_cat = [];       // 부재 카테고리
+
+        // 8. 제작처 구분 (선택)
+        let productSite_name = [];
+
+        // 9. 제작 번호 (선택)
+        let productNum_name = [];
+
+        // 여기서부터는 파일을 읽어옴
+        const selectedFiles = csvButton.files;
+
+        if(selectedFiles.length == 0)
+            return;
+
+        let file = selectedFiles[0];
+        let reader = new FileReader();
+
+        reader.onload = function(ev) {
+            const file = ev.target.result;
+            const allLines = file.split(/\r\n|\n/);
+            allLines.forEach((line) => {
+                //console.log(line);
+            })
+        }
+
+        reader.readAsText(file, "euc-kr");
+    }
 }
--- MaxBIM-app/module/exportQuantities.js
+++ MaxBIM-app/module/exportQuantities.js
@@ -1,38 +1,38 @@
-const fileButton = document.getElementById ("file");
-const titleInput = document.getElementById ("title");
-const dateInput = document.getElementById ("date");
-const saveButton = document.getElementById ("save");
+const fileButton = document.getElementById("file");
+const titleInput = document.getElementById("title");
+const dateInput = document.getElementById("date");
+const saveButton = document.getElementById("save");
 
-saveButton.addEventListener ("click", saveXLSX);
+saveButton.addEventListener("click", saveXLSX);
 
-function saveXLSX () {
+function saveXLSX() {
     // 타이틀, 날짜 정보를 가져옴
     //console.log (`제목: ${titleInput.value}, 날짜: ${dateInput.value}`);
 
     const selectedFiles = fileButton.files;
 
-    if (selectedFiles.length == 0) {
+    if(selectedFiles.length == 0) {
         alert ("중간보고서 txt 파일 선택은 필수입니다.");
         return;
     }
 
-    for (const file of selectedFiles) {
-        if (file.type == "text/plain") {
+    for(const file of selectedFiles) {
+        if(file.type == "text/plain") {
             // ================================================== 파일명 읽어오기
-            //console.log (`파일명: ${file.name}\n`);
-            let excelFileName = file.name.replace (/.txt/gi, '.xlsx');
-            excelFileName = excelFileName.replace (' - 선택한 부재 정보 (중간보고서)', '');
-            excelFileName = excelFileName.replace (' - Graphisoft ArchiCAD-64 19', '');
+            //console.log(`파일명: ${file.name}\n`);
+            let excelFileName = file.name.replace(/.txt/gi, '.xlsx');
+            excelFileName = excelFileName.replace(' - 선택한 부재 정보 (중간보고서)', '');
+            excelFileName = excelFileName.replace(' - Graphisoft ArchiCAD-64 19', '');
 
             // ================================================== 텍스트 파일 읽어오기
             let textContents;
-            let reader = new FileReader ();
-            reader.onload = function (ev) {
+            let reader = new FileReader();
+            reader.onload = function(ev) {
                 textContents = ev.target.result;    // 텍스트 파일 내용 저장
 
                 // 엑셀 파일 생성 (SheetJS 무료 버전에서는 셀 스타일 지정은 불가함)
-                let wb = XLSX.utils.book_new ();    // workbook 생성
-                wb.SheetNames.push ("Sheet 1");     // 시트 생성
+                let wb = XLSX.utils.book_new();    // workbook 생성
+                wb.SheetNames.push("Sheet 1");     // 시트 생성
                 
                 let iBegin;
                 let iEnd;
@@ -43,54 +43,54 @@ function saveXLSX () {
                 // 기록할 데이터는 2차원 배열로 입력하면 됨, let wsData = [[],['A1' , 'A2', 'A3'],['B1','B2','B3'],['C1','C2']];
                 // 실제 기록할 데이터
                 let wsData = [ [titleInput.value], [], ['','','','','','',dateInput.value], ['구간', '품목', '규격', '길이', '수량', '단위', '비고'] ];     // 헤더
-                let strArr = textContents.split ('\n');
-                for (let i=0 ; i < strArr.length ; i++) {
-                    let smallStrArr = strArr [i].split ('|');
+                let strArr = textContents.split('\n');
+                for(let i=0 ; i < strArr.length ; i++) {
+                    let smallStrArr = strArr[i].split('|');
 
-                    for (let j=0 ; j < smallStrArr.length ; j++) {
-                        smallStrArr [j] = smallStrArr [j].trim ();
+                    for(let j=0 ; j < smallStrArr.length ; j++) {
+                        smallStrArr[j] = smallStrArr[j].trim();
                     }
 
                     // 1번째 줄의 품목은 무조건 저장
-                    if (i == 0) {
+                    if(i == 0) {
                         iBegin = iEnd = i;
-                        productName = smallStrArr [0];
+                        productName = smallStrArr[0];
                     } else {
-                        if (productName === smallStrArr [0]) {
+                        if (productName === smallStrArr[0]) {
                             iEnd = i;
                         } else {
                             // 셀 병합 정보 추가
-                            merge.push ({ s: { r: iBegin + 4, c: 1 }, e: { r: iEnd + 4, c: 1 } },);     // 품목
-                            merge.push ({ s: { r: iBegin + 4, c: 5 }, e: { r: iEnd + 4, c: 5 } },);     // 단위
+                            merge.push({ s: { r: iBegin + 4, c: 1 }, e: { r: iEnd + 4, c: 1 } },);     // 품목
+                            merge.push({ s: { r: iBegin + 4, c: 5 }, e: { r: iEnd + 4, c: 5 } },);     // 단위
 
                             iBegin = iEnd = i;
-                            productName = smallStrArr [0];
+                            productName = smallStrArr[0];
                         }
                     }
 
                     // 비어 있는 행은 처리하지 않음
-                    if (strArr [i].length > 12) {
-                        wsData.push (['', smallStrArr [0], smallStrArr [1], smallStrArr [2], parseInt (smallStrArr [4]), smallStrArr [3]]);     // 수량만 숫자로 변환
+                    if (strArr[i].length > 12) {
+                        wsData.push(['', smallStrArr [0], smallStrArr [1], smallStrArr [2], parseInt (smallStrArr [4]), smallStrArr [3]]);     // 수량만 숫자로 변환
                     }
                 }
 
                 // 데이터를 엑셀 파일로 저장함
-                let ws = XLSX.utils.aoa_to_sheet (wsData);
-                ws ["!merges"] = merge;
-                wb.Sheets ["Sheet 1"] = ws;
-                wb ["Sheets"]["Sheet 1"]["!cols"] = [{wpx: 40}, {wpx: 170}, {wpx: 130}, {wpx: 70}, {wpx: 70}, {wpx: 70}, {wpx: 70}];    // 열 너비 지정
-                let wbout = XLSX.write (wb, {bookType:'xlsx', type:'binary'});
-                saveAs (new Blob ([s2ab (wbout)], {type:"application/octet-stream"}), excelFileName);
+                let ws = XLSX.utils.aoa_to_sheet(wsData);
+                ws["!merges"] = merge;
+                wb.Sheets["Sheet 1"] = ws;
+                wb["Sheets"]["Sheet 1"]["!cols"] = [{wpx: 40}, {wpx: 170}, {wpx: 130}, {wpx: 70}, {wpx: 70}, {wpx: 70}, {wpx: 70}];    // 열 너비 지정
+                let wbout = XLSX.write(wb, {bookType:'xlsx', type:'binary'});
+                saveAs(new Blob([s2ab (wbout)], {type:"application/octet-stream"}), excelFileName);
             };
-            reader.readAsText (file, "euc-kr");
+            reader.readAsText(file, "euc-kr");
         }
     }
 }
 
 // 파일 저장 루틴
-function s2ab (s) {
-    let buf = new ArrayBuffer (s.length);
-    let view = new Uint8Array (buf);
-    for (let i=0 ; i < s.length ; i++) view [i] = s.charCodeAt(i) & 0xFF;
+function s2ab(s) {
+    let buf = new ArrayBuffer(s.length);
+    let view = new Uint8Array(buf);
+    for(let i=0 ; i < s.length ; i++) view[i] = s.charCodeAt(i) & 0xFF;
     return buf;
 }
